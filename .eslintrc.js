273c4f68d8bb6d2f3e05bbffd28f2850cef41cd1
Jehan <gaozhiheng_email@163.com>
2020-07-11 01:19:28 +0800

    chore: set eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,16 @@
 module.exports = {
-  //此项是用来告诉eslint找当前配置文件不能往父级查找
-  root: true, 
-  //此项是用来指定eslint解析器的，解析器必须符合规则，babel-eslint解析器是对babel解析器的包装使其与ESLint解析
+  root: true,
   parser: 'babel-eslint',
-  //此项是用来指定javaScript语言类型和风格，sourceType用来指定js导入的方式，默认是script，此处设置为module，指某块导入方式
-  // 此项是用来配置标准的js风格，就是说写代码的时候要规范的写，如果你使用vs-code我觉得应该可以避免出错
   extends: 'standard',
-  // add your custom rules here
-  // 下面这些rules是用来设置从插件来的规范代码的规则，使用必须去掉前缀eslint-plugin-
-  // 主要有如下的设置规则，可以设置字符串也可以设置数字，两者效果一致
-  // "off" -> 0 关闭规则
-  // "warn" -> 1 开启警告规则
-  //"error" -> 2 开启错误规则
-  // 了解了上面这些，下面这些代码相信也看的明白了
-  'rules': {
-      // allow paren-less arrow functions
-      'arrow-parens': 0,
-      // allow async-await
-      'generator-star-spacing': 0,
-      // allow debugger during development
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+  rules: {
+    eqeqeq: [0],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-tabs': 'off',
+    'space-before-function-paren': 0,
+    'no-undef': 0,
+  },
 }
